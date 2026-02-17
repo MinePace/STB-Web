420c4904d3fa5e3766c25ba89b9caa0739b46f67
Joey Zwinkels <joeyzwinkels@gmail.com>
2026-02-17 19:35:32 +0100

    Update ChampionshipPage.jsx

--- STB_Frontend/src/Pages/Championship/ChampionshipPage.jsx
+++ STB_Frontend/src/Pages/Championship/ChampionshipPage.jsx
@@ -44,7 +44,7 @@ function ChampionshipPage() {
     setLoading(true);
     setNotFound(false);
 
-    fetch(`https://stbleaguedata.vercel.app/api/championship/races/${season}/${division}`)
+    fetch(`https://stbleaguedata.vercel.app/api/Championship/${season}/${division}`)
       .then((res) => res.json())
       .then((raceData) => {
         if (!Array.isArray(raceData) || raceData.length === 0) {
