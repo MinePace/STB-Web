93d3493b9f3cd6946813734ec3754935943a3432
Joey Zwinkels <joeyzwinkels@gmail.com>
2026-05-01 22:43:49 +0200

    Update AddRaceResultPage.jsx

--- STB_Frontend/src/Pages/Admin/AddRaceResultPage.jsx
+++ STB_Frontend/src/Pages/Admin/AddRaceResultPage.jsx
@@ -393,6 +393,7 @@ export default function AddRaceResults() {
       Pos_Change: r.pos_Change,
       FastestLap: r.fastestLap,
       Time: r.raceTime.trim(),
+      TimePenalty: r.raceTime.trim() && r.dnf === "No" ? r.raceTime.trim() : null,
     }))
 
     const url = "https://stbleaguedata.vercel.app/api/race/raceresult";
